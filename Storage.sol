127833f54e5f0e3aa8362240b1ea21053c0db55b
shweta005 <shwetadraekar04@gmail.com>
2021-06-02 18:41:13 +0530

    Registration completed for both voter & candidate

--- Storage.sol
+++ Storage.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.8.0;
 
 contract Storage{
-    
+    //Voter's Data
     struct Voter{
         string name;
         uint8  age;
@@ -11,6 +11,7 @@ contract Storage{
         bool isActive;
     }
     
+    //Candidate's Data
     struct Candidate{
         string  name;
         uint8   age;
@@ -20,18 +21,35 @@ contract Storage{
         address cadd;
     }
     
+    //counter variables
     uint256 public voterCounter;
     uint256 public candidateCounter;
     
-    mapping(address =>Voter) public voter;
-    mapping(address=>Candidate) public  candidate;
     
+    mapping(address =>Voter) public voter; //mapping for voter
+    mapping(address=>Candidate) public  candidate;   //mapping for candidate
+    
+    
+    //modifier to check that voter and candidates cannot register with same address
     modifier registered{
       require(msg.sender != candidate[msg.sender].cadd,"try another address ");  
         _;
     }
     
-    function registerVoter(string memory _name, uint8 _age) registered  public  {
+    //modifier to check voter is exist or not
+     modifier checkVoter(){
+    require(voter[msg.sender].id != 0,"Voter doesn't exist");
+            _;
+    }
+    //modifier to check candidate is exist or not
+     modifier checkCandidate(){
+    require(candidate[msg.sender].cid != 0,"Candidate doesn't exist");
+            _;
+    }
+    
+    
+    //register voter
+    function RegisterVoter(string memory _name, uint8 _age) registered  public  {
         voterCounter++;
         Voter memory vot;
         vot.id      = voterCounter; 
@@ -42,19 +60,14 @@ contract Storage{
         voter[msg.sender] = vot;
     }
         
-    modifier checkVoter(){
-    require(voter[msg.sender].id != 0,"Voter doesn't exist");
-            _;
-    }
-    
- 
+   //function to set voter active
     function setVoterActive(address _add) checkVoter public{
         require(voter[_add].age >= 18 ,"You age must be 18");
         voter[_add].isActive = true;
-        
     }
     
-    function registerCandidate(string memory _cname, uint8 _cage) public{
+        //register candidate
+    function RegisterCandidate(string memory _cname, uint8 _cage) public{
         candidateCounter++;
         Candidate memory can;
         can.cid         = candidateCounter;
@@ -64,9 +77,11 @@ contract Storage{
         can.isCandidate = true;
         candidate[msg.sender] = can;
     }
-    function setCandActive(address _add) checkVoter public{
-        require(candidate[_add].age >= 18 ,"You age must be 18");
-        candidate[_add].iscActive = true;
+    
+    //function to set voter active
+    function setCandActive(address add) checkCandidate  public{
+        require(candidate[add].age >= 18 ,"You age must be 18");
+        candidate[add].iscActive = true;
     }
     
     
